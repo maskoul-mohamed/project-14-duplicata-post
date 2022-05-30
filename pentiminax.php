347fa07dd4584ef2aebdad5314dd9ef1904d2c8d
Mohamed Maskoul <66232435+maskoul-mohamed@users.noreply.github.com>
2022-05-30 10:14:25 +0000

    Add files

---
+++ pentiminax.php
@@ -0,0 +1,18 @@
+<?php
+
+/*
+Plugin Name: dupliquer
+Version: 0.1
+Author: Maskoul Mohamed
+*/
+
+use Pentiminax\DuplicatePost\PentiminaxPlugin;
+
+if ( ! defined( 'ABSPATH' ) )
+	exit;
+
+define('PENTIMINAX_PLUGIN_DIR', plugin_dir_path(__FILE__));
+
+require PENTIMINAX_PLUGIN_DIR . 'vendor/autoload.php';
+
+$plugin = new PentiminaxPlugin(__FILE__);
